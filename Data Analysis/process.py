86cf4bd0f6274889bb0c5a5c0fb79bf808718ad9
leerubi <dj112200@gmail.com>
2018-11-17 16:01:49 +0900

    merged two csvs
    should merged flightsCimlbed into it

--- Data Analysis/process.py
+++ Data Analysis/process.py
@@ -1,7 +1,6 @@
 from manager import load_data
-from utils import merge_same_time_data, merge_two_csvs, merge_two_csvs
+from utils import merge_same_time_data, merge_two_csvs
 
-# load_data()
-# merge_same_time_data()
-
-# merge_two_csvs()
+load_data()
+merge_same_time_data()
+merge_two_csvs()
--- Data Analysis/utils.py
+++ Data Analysis/utils.py
@@ -1,6 +1,8 @@
 import os
+import csv
 import pandas as pd
-import numpy as np
+import datetime
+from dateutil.parser import parse
 
 def merge_same_time_data():
 
@@ -15,71 +17,34 @@ def merge_same_time_data():
         data = pd.read_csv(root_path_from + file)
         processed_data = data.groupby(['date'], as_index=False).mean()
         f_processed = root_path_to + file[:-4] + "_processed.csv"
+
         processed_data.to_csv(f_processed)
 
 def merge_two_csvs():
 
-    # 시간 간격이 10초 이상일 때는 interpolation하지 않는 기능 추후 구현
-
     root_path_from = 'iPhone Data/csvs/processed_csvs/'
     root_path_to = 'iPhone Data/csvs/merged_csvs/'
-    file_list = os.listdir(root_path_from)
+    file_list = os.listdir('iPhone Data/Location/')
 
     for file in file_list:
 
         if file == '.DS_Store': continue
 
-        data = pd.read_csv(root_path_from + file)
+        f_gps = root_path_from + file[:-4] + "_gps_processed.csv"
+        f_sensor = root_path_from + file[:-4] + "_sensor_processed.csv"
+        f_merged = root_path_to + file[:-4] + "_merged.csv"
 
-        '''
-        1. 제목 로우와 시간 칼럼을 만든다
-        2. 있는 데이터를 넣는다 (A, B 둘다)
-        3. interpolate()
-        '''
+        data_gps = pd.read_csv(f_gps)
+        data_sensor = pd.read_csv(f_sensor)
 
+        data_merged = data_gps.merge(data_sensor, left_on='date', right_on='date', how='outer')
 
+        data_merged = data_merged.sort_values('date').reset_index(drop=False)
+        data_merged = data_merged.interpolate()
+        data_merged = data_merged.dropna()
 
-        #
-        # range = np.arange(data['date'].values[0], data['date'].values[-1])
-        #
-        # for elem in range:
-        #     if elem not in np.array(data['date']):
-        #         data = data.append({'date': elem}, ignore_index=True)
-        #
-        # data = data.sort_values('date').reset_index(drop=False)
-        # data = data.interpolate()
-        #
-        # f_interpolated = root_path_to + file[:-14] + "_interpolated.csv"
-        # data.to_csv(f_interpolated)
+        data_merged.to_csv(f_merged)
 
-# def merge_two_csvs():
-#     print("implement!")
-#
-#     root_path_from = 'iPhone Data/interpolated_csvs/'
-#     root_path_to = 'iPhone Data/merged_csvs/'
-#     file_list = os.listdir('iPhone Data/Location/')
-#
-#     for file in file_list:
-#
-#         if file == '.DS_Store': continue
-#
-#         f_gps = root_path_from + file[:-4] + "_gps_interpolated.csv"
-#         f_sensor = root_path_from + file[:-4] + "_sensor_interpolated.csv"
-#
-#         data_gps = pd.read_csv(f_gps)
-#         range_gps = np.arange(data_gps['time'].values[0], data_gps['time'].values[-1] + 1)
-#
-#         data_sensor = pd.read_csv(f_sensor)
-#         range_sensor = np.arange(data_sensor['time'].values[0], data_sensor['time'].values[-1] + 1)
-#
-#         range_inter = np.intersect1d(range_gps, range_sensor)
-#         data_gps = data_gps.loc[data_gps['time'] >= range_inter[0]]
-#         data_gps = data_gps.loc[data_gps['time'] <= range_inter[-1]]
-#         data_sensor = data_sensor.loc[data_sensor['time'] >= range_inter[0]]
-#         data_sensor = data_sensor.loc[data_sensor['time'] <= range_inter[-1]]
-#
-#         merged_data = pd.merge(data_gps, data_sensor, left_index=True, right_index=True)
-#         print("fds")
 
 
 def impute_empty_time_data():
