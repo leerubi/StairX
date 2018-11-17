2f4458dae1476f6facaf54307890d8b4da75ee81
leerubi <dj112200@gmail.com>
2018-11-17 11:42:09 +0900

    implementing merge_two_csvs

--- Data Analysis/utils.py
+++ Data Analysis/utils.py
@@ -17,12 +17,12 @@ def merge_same_time_data():
         f_processed = root_path_to + file[:-4] + "_processed.csv"
         processed_data.to_csv(f_processed)
 
-def interpolation():
+def merge_two_csvs():
 
     # 시간 간격이 10초 이상일 때는 interpolation하지 않는 기능 추후 구현
 
-    root_path_from = 'iPhone Data/processed_csvs/'
-    root_path_to = 'iPhone Data/interpolated_csvs/'
+    root_path_from = 'iPhone Data/csvs/processed_csvs/'
+    root_path_to = 'iPhone Data/csvs/merged_csvs/'
     file_list = os.listdir(root_path_from)
 
     for file in file_list:
@@ -30,46 +30,56 @@ def interpolation():
         if file == '.DS_Store': continue
 
         data = pd.read_csv(root_path_from + file)
-        range = np.arange(data['time'].values[0], data['time'].values[-1])
 
-        for elem in range:
-            if elem not in np.array(data['time']):
-                data = data.append({'time': elem}, ignore_index=True)
+        '''
+        1. 제목 로우와 시간 칼럼을 만든다
+        2. 있는 데이터를 넣는다 (A, B 둘다)
+        3. interpolate()
+        '''
 
-        data = data.sort_values('time').reset_index(drop=False)
-        data = data.interpolate()
 
-        f_interpolated = root_path_to + file[:-14] + "_interpolated.csv"
-        data.to_csv(f_interpolated)
 
-def merge_two_csvs():
-    print("implement!")
+        #
+        # range = np.arange(data['date'].values[0], data['date'].values[-1])
+        #
+        # for elem in range:
+        #     if elem not in np.array(data['date']):
+        #         data = data.append({'date': elem}, ignore_index=True)
+        #
+        # data = data.sort_values('date').reset_index(drop=False)
+        # data = data.interpolate()
+        #
+        # f_interpolated = root_path_to + file[:-14] + "_interpolated.csv"
+        # data.to_csv(f_interpolated)
 
-    root_path_from = 'iPhone Data/interpolated_csvs/'
-    root_path_to = 'iPhone Data/merged_csvs/'
-    file_list = os.listdir('iPhone Data/Location/')
-
-    for file in file_list:
-
-        if file == '.DS_Store': continue
-
-        f_gps = root_path_from + file[:-4] + "_gps_interpolated.csv"
-        f_sensor = root_path_from + file[:-4] + "_sensor_interpolated.csv"
-
-        data_gps = pd.read_csv(f_gps)
-        range_gps = np.arange(data_gps['time'].values[0], data_gps['time'].values[-1] + 1)
-
-        data_sensor = pd.read_csv(f_sensor)
-        range_sensor = np.arange(data_sensor['time'].values[0], data_sensor['time'].values[-1] + 1)
-
-        range_inter = np.intersect1d(range_gps, range_sensor)
-        data_gps = data_gps.loc[data_gps['time'] >= range_inter[0]]
-        data_gps = data_gps.loc[data_gps['time'] <= range_inter[-1]]
-        data_sensor = data_sensor.loc[data_sensor['time'] >= range_inter[0]]
-        data_sensor = data_sensor.loc[data_sensor['time'] <= range_inter[-1]]
-
-        merged_data = pd.merge(data_gps, data_sensor, left_index=True, right_index=True)
-        print("fds")
+# def merge_two_csvs():
+#     print("implement!")
+#
+#     root_path_from = 'iPhone Data/interpolated_csvs/'
+#     root_path_to = 'iPhone Data/merged_csvs/'
+#     file_list = os.listdir('iPhone Data/Location/')
+#
+#     for file in file_list:
+#
+#         if file == '.DS_Store': continue
+#
+#         f_gps = root_path_from + file[:-4] + "_gps_interpolated.csv"
+#         f_sensor = root_path_from + file[:-4] + "_sensor_interpolated.csv"
+#
+#         data_gps = pd.read_csv(f_gps)
+#         range_gps = np.arange(data_gps['time'].values[0], data_gps['time'].values[-1] + 1)
+#
+#         data_sensor = pd.read_csv(f_sensor)
+#         range_sensor = np.arange(data_sensor['time'].values[0], data_sensor['time'].values[-1] + 1)
+#
+#         range_inter = np.intersect1d(range_gps, range_sensor)
+#         data_gps = data_gps.loc[data_gps['time'] >= range_inter[0]]
+#         data_gps = data_gps.loc[data_gps['time'] <= range_inter[-1]]
+#         data_sensor = data_sensor.loc[data_sensor['time'] >= range_inter[0]]
+#         data_sensor = data_sensor.loc[data_sensor['time'] <= range_inter[-1]]
+#
+#         merged_data = pd.merge(data_gps, data_sensor, left_index=True, right_index=True)
+#         print("fds")
 
 
 def impute_empty_time_data():
