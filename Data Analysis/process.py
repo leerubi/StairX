b93f50e7b3c23302cebd52fdffaec2ed82025567
leerubi <dj112200@gmail.com>
2018-11-18 09:05:53 +0900

    init step2

--- Data Analysis/process.py
+++
@@ -1,9 +0,0 @@
-from manager import load_location, load_flightsClimbed
-from utils import merge_same_time_data, merge_two_csvs, merge_Location_and_FlightsClimbed
-
-# load_data()
-# merge_same_time_data()
-# merge_two_csvs()
-# load_flightsClimbed()
-
-merge_Location_and_FlightsClimbed()
