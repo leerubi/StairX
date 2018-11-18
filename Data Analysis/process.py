703f9cd7cc965c1da36754d58ee89d46171dd18b
leerubi <dj112200@gmail.com>
2018-11-18 09:21:02 +0900

    return to normal state

--- Data Analysis/process.py
+++ Data Analysis/process.py
@@ -2,9 +2,9 @@ from manager import load_location, load_flightsClimbed
 from utils import merge_same_time_data, merge_two_csvs, merge_Location_and_FlightsClimbed
 
 
-# load_location()
-# merge_same_time_data()
-# merge_two_csvs()
-#
-# load_flightsClimbed()
+load_location()
+merge_same_time_data()
+merge_two_csvs()
+
+load_flightsClimbed()
 merge_Location_and_FlightsClimbed()
