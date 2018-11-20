dd3b805a08ad6e5f4106efc16dfcb83890d1f26a
leerubi <dj112200@gmail.com>
2018-11-20 23:46:10 +0900

    arrange master branch

---
+++ Etc/Flights Climbed/Flights ClimbedTests/Flights_ClimbedTests.swift
@@ -0,0 +1,34 @@
+//
+//  Flights_ClimbedTests.swift
+//  Flights ClimbedTests
+//
+//  Created by 이다진 on 12/11/2018.
+//  Copyright © 2018 이다진. All rights reserved.
+//
+
+import XCTest
+@testable import Flights_Climbed
+
+class Flights_ClimbedTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
